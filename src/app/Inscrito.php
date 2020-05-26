4dfb11a2a678b40e48e02fd932190d30f7b31d74
mauriciogerbersc <mauricio.gerber@gmail.com>
2020-05-26 14:07:03 -0300

    resolvendo problemas

--- src/app/Inscrito.php
+++ src/app/Inscrito.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Inscrito extends Model
-{
+{   
+    protected $table = "inscritoo";
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
